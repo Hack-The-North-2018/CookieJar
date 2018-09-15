7107f45e4fef874fe75cb416ee3dcd78bda2c7d7
OustanDing <oustan10@gmail.com>
2018-09-15 15:50:36 -0400

    initialized firebase

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ app.config['SESSION_FILE_DIR'] = mkdtemp()
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 
+# Firebase Database
+cred = credentials.Certificate('cookiejar-tor1-firebase-adminsdk-frm0v-bfbd8235b5.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://cookiejar-tor1.firebaseio.com/'
+    })
+
+root = db.reference()
+
 @app.route('/')
 def index():
     return 'Hello world!'
