7cdf214bbc4ed65bd5697c1d6a9fc18adbdcbca8
OustanDing <oustan10@gmail.com>
2018-09-15 15:40:47 -0400

    initialized app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import *
+from tempfile import mkdtemp
+
+app = Flask(__name__)
+
+app.secret_key = 'caffeine'
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SESSION_FILE_DIR'] = mkdtemp()
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+
+@app.route('/')
+def index():
+    return 'Hello world!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
