d4295f44ead47084a4301fe0724f1ff44df0eae7
OustanDing <oustan10@gmail.com>
2018-09-16 00:41:29 -0400

    push

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,7 @@
 function replace(form) {
     members = form.members.value;
+    memberlist = members.split(" ")
     if (members.substr(members.length - 1) == " ") {
-
+        if (memberlist[-1])
     }
 }
